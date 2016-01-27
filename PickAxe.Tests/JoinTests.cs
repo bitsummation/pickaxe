21454e57e6ce67093503f6cd930925e0f8b27dae
breeve <brock.reeve@gmail.com>
2016-01-27 09:16:06 -0600

    more test

---
+++ PickAxe.Tests/JoinTests.cs
@@ -0,0 +1,66 @@
+﻿/* Copyright 2015 Brock Reeve
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PickAxe.Tests
+{
+    [TestFixture]
+    public class JoinTests
+    {
+        [Test]
+        public void Join_Two_SelectArgs()
+        {
+
+            var code = @"
+
+   create buffer a (id int, name string)
+create buffer b (id int, name string)
+
+insert into a
+select 1, 'first'
+
+insert into a
+select 2, 'first'
+
+insert into b
+select 1, 'second'
+
+select a.name, b.name
+from a
+join b on a.id = b.id
+
+";
+            var runable = TestHelper.Compile(code, null);
+
+            int called = 0;
+            runable.Select += (table) =>
+            {
+                called++;
+                Assert.IsTrue(table.Columns().Length == 2);
+                Assert.IsTrue(table.RowCount == 1);
+
+                Assert.IsTrue(table[0][0].ToString() == "first");
+                Assert.IsTrue(table[0][1].ToString() == "second");
+            };
+
+            runable.Run();
+            Assert.IsTrue(called == 1);
+        }
+    }
+}
