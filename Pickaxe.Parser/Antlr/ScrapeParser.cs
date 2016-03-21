1e2e6e3fec6ffa918143c122b0a282ae1fac24e3
breeve <brock.reeve@gmail.com>
2016-03-20 21:35:21 -0500

    new threaded table

--- Pickaxe.Parser/Antlr/ScrapeParser.cs
+++ Pickaxe.Parser/Antlr/ScrapeParser.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.5.2 Scrape.g 2016-02-24 21:35:12
+// $ANTLR 3.5.2 Scrape.g 2016-03-20 21:31:26
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
---
+++ Pickaxe.Runtime/ThreadedDownloadPageTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pickaxe.Runtime
+{
+    internal class ThreadedDownloadPageTable : RuntimeTable<DownloadPage>
+    {
+
+    }
+}
