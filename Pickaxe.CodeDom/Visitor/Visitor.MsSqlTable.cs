dd87304248fec3ce1d18650ab8e6194c77f97e4a
breeve <brock.reeve@gmail.com>
2016-04-27 21:27:45 -0500

    sql working

--- Pickaxe.CodeDom/Visitor/Visitor.MsSqlTable.cs
+++ Pickaxe.CodeDom/Visitor/Visitor.MsSqlTable.cs
@@ -12,19 +12,73 @@
  * limitations under the License.
  */
 
+using Pickaxe.CodeDom.Semantic;
+using Pickaxe.Runtime;
 using Pickaxe.Sdk;
 using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Pickaxe.CodeDom.Visitor
 {
     public partial class CodeDomGenerator : IAstVisitor
     {
-        public void Visit(MsSqlTable literal)
+        public void Visit(MsSqlTable table)
         {
+            var descriptor = new TableDescriptor(typeof(MsSqlTable<>));
+            var sqlTable = new CodeTypeDeclaration(table.Variable) { TypeAttributes = TypeAttributes.NestedPrivate };
+            sqlTable.BaseTypes.Add(new CodeTypeReference("IRow"));
+
+            var sqlTableCodeDomType = new CodeTypeReference("MsSqlTable", new CodeTypeReference(table.Variable));
+            Scope.Current.Type.Type.Members.Add(
+                new CodeMemberField(sqlTableCodeDomType, table.Variable) { Attributes = MemberAttributes.Public | MemberAttributes.Final });
+
+            Scope.Current.Type.Constructor.Statements.Add(new CodeAssignStatement(
+               new CodeSnippetExpression(table.Variable),
+               new CodeObjectCreateExpression(
+                   new CodeTypeReference("MsSqlTable", new CodeTypeReference(table.Variable)))));
+
+            var codeExpressions = new List<CodeExpression>();
+            foreach (var arg in table.Args)
+            {
+                var domArg = VisitChild(arg);
+                sqlTable.Members.AddRange(domArg.ParentMemberDefinitions);
+                codeExpressions.Add(new CodePrimitiveExpression(arg.Variable));
+                descriptor.Variables.Add(new VariableTypePair { Variable = arg.Variable, Primitive = TablePrimitive.FromString(arg.Type) });
+            }
+
+            _mainType.Type.Members.Add(sqlTable);
+
+            if (Scope.Current.IsCurrentScopeRegistered(table.Variable))
+                Errors.Add(new VariableAlreadyExists(new Semantic.LineInfo(table.Line.Line, table.Line.CharacterPosition), table.Variable));
+
+            Scope.Current.RegisterTable(table.Variable, descriptor, sqlTableCodeDomType);
+
+            //Init Code
+            CodeMemberMethod method = new CodeMemberMethod();
+            method.Name = "Init_" + sqlTable.Name;
+            method.Attributes = MemberAttributes.Private;
+
+            _mainType.Type.Members.Add(method);
+
+            var assignment = new CodeAssignStatement(new CodeVariableReferenceExpression("_" + Scope.Current.ScopeIdentifier + "." + table.Variable + ".ConnectionString"), new CodePrimitiveExpression(table.ConnectionString));
+            method.Statements.Add(assignment);
+
+            assignment = new CodeAssignStatement(new CodeVariableReferenceExpression("_" + Scope.Current.ScopeIdentifier + "." + table.Variable + ".Table"), new CodePrimitiveExpression(table.Table));
+            method.Statements.Add(assignment);
+
+            assignment = new CodeAssignStatement(new CodeVariableReferenceExpression("_" + Scope.Current.ScopeIdentifier + "." + table.Variable + ".FieldNames"),
+                new CodeArrayCreateExpression(new CodeTypeReference(typeof(string)), codeExpressions.ToArray()));
+            method.Statements.Add(assignment);
+
+            var methodcall = new CodeMethodInvokeExpression(
+               new CodeMethodReferenceExpression(null, method.Name));
+
+            _codeStack.Peek().ParentStatements.Add(methodcall);
+            _codeStack.Peek().CodeExpression = methodcall;
         }
     }
 }
--- Pickaxe.Parser/Antlr/ScrapeParser.cs
+++ Pickaxe.Parser/Antlr/ScrapeParser.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.5.2 Scrape.g 2016-04-23 16:53:44
+// $ANTLR 3.5.2 Scrape.g 2016-04-23 17:02:25
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
