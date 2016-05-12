beea97ed4f25f3a366cf3544adfc7a65f5af555d
breeve <brock.reeve@gmail.com>
2016-05-11 20:55:00 -0500

    change example

--- Examples/youtube.s
+++ Examples/youtube.s
@@ -1,43 +1,31 @@
 create buffer t(value int)
 
+//used for loop count
 insert into t
 select *
-from expand (1 to 2)
+from expand (1 to 3)
 
-create buffer videos(url string, link string, title string, views string, duration string, processed int)
-insert into videos
-select
-	url,
-	'http://youtube.com' + pick '.content-wrapper a' take attribute 'href',
-	pick '.content-wrapper a span.title',
-	pick '.content-wrapper .view-count',
-	pick '.thumb-wrapper .video-time',
-	0
-from download page 'https://www.youtube.com/watch?v=7NJqUN9TClM'
-where nodes = 'div.watch-sidebar-body .video-list-item'
+create buffer videos(url string, link string, title string, views int, duration string)
 
 create buffer urls(url string)
 
+insert into urls
+select 'https://www.youtube.com/watch?v=7NJqUN9TClM'
+
 each(var i in t)
 {
-	insert overwrite urls
-	select url
-	from videos
-	where processed = 0
-	
 	insert into videos
 	select
 		url,
 		'http://youtube.com' + pick '.content-wrapper a' take attribute 'href',
 		pick '.content-wrapper a span.title',
-		pick '.content-wrapper .view-count',
-		pick '.thumb-wrapper .video-time',
-		0
-	from download page (select link from videos where processed = 0) with (thread(10))
-	where nodes = 'div.watch-sidebar-body .video-list-item'
+		pick '.content-wrapper .view-count' match '\d+',
+		pick '.thumb-wrapper .video-time'
+	from download page (select url from urls) with (thread(10))
+	where nodes = 'div.watch-sidebar-body .video-list-item'		
 	
-	update v
-	set processed = 1
+	insert overwrite urls
+	select link
 	from videos v
 	join urls on v.url = urls.url
 }
