7a63ea6211d2830ba7ee8e30204299d21e10d336
breeve <brock.reeve@gmail.com>
2016-12-15 23:11:27 -0600

    fix example

--- Examples/LakeTravisWaterLevel.s
+++ Examples/LakeTravisWaterLevel.s
@@ -4,7 +4,7 @@ insert into levels
 select
 	pick 'td:nth-child(1) p.bold' match '(\d+)-(\w+)' replace '$2',
 	pick 'td:nth-child(1) p.bold' match '(\d+)-(\w+)' replace '$1',
-	pick 'td:nth-child(2) p' match '\d{3}\.\d{2}'
+	pick 'td:nth-child(2)' match '\d{3}\.\d{2}'
 from download page (
 	select
 		'http://www.golaketravis.com/waterlevel/' + pick '' take attribute 'href'
