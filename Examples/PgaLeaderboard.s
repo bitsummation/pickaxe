bc6ccb2340f8ca6ab7bdf13ed5b31029bbffc172
breeve <brock.reeve@gmail.com>
2017-04-17 14:58:04 -0500

    update example

--- Examples/PgaLeaderboard.s
+++ Examples/PgaLeaderboard.s
@@ -1,8 +1,8 @@
 select
 	pick 'td:nth-child(2)' as position,
 	pick 'td:nth-child(5) .hidden-small' as player,
-	pick 'td:nth-child(6)' match '\s+' replace '' as score,
-	pick 'td:nth-child(7)' match '\s+' replace '' as hole,
-	pick 'td:nth-child(8)' match '\s+' replace '' as round
+	pick 'td:nth-child(6)' as score,
+	pick 'td:nth-child(7)' as hole,
+	pick 'td:nth-child(8)' as round
 from download page 'http://www.pgatour.com/leaderboard.html' with (js)
 where nodes = '.leaderboard-item tr.row-main'
